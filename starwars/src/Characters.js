3a3f587ded26b3d1a12d93350ca68f280ce70c99
jackgipson <jackjgipson@gmail.com>
2019-03-23 11:54:56 -0700

    characters component not working

---
+++ starwars/src/Characters.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const characters = [
+  {
+    birth_year: "19BBY",
+    created: "2014-12-09T13:50:51.644000Z",
+    edited: "2014-12-20T21:17:56.891000Z",
+    eye_color: "blue",
+    films: [],
+    gender: "male",
+    hair_color: "blond",
+    height: "172",
+    homeworld: "https://swapi.co/api/planets/1/",
+    mass: "77",
+    name: "Luke Skywalker",
+    skin_color: "fair",
+    species: [],
+    starships: [],
+    url: "https://swapi.co/api/people/1/",
+    vehicles: []
+  }
+];
+
+export default characters;
