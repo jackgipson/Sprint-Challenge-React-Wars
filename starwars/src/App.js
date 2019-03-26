9c1e9f1ee0570c57eb8be0954a05708611aeb409
jackgipson <jackjgipson@gmail.com>
2019-03-25 20:53:05 -0700

    need more help

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Characters from './Characters';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       starwarsChars: []
     };
--- starwars/src/Characters.js
+++ starwars/src/Characters.js
@@ -1,24 +1,10 @@
 import React from "react";
 
-const characters = [
-  {
-    birth_year: "19BBY",
-    created: "2014-12-09T13:50:51.644000Z",
-    edited: "2014-12-20T21:17:56.891000Z",
-    eye_color: "blue",
-    films: [],
-    gender: "male",
-    hair_color: "blond",
-    height: "172",
-    homeworld: "https://swapi.co/api/planets/1/",
-    mass: "77",
-    name: "Luke Skywalker",
-    skin_color: "fair",
-    species: [],
-    starships: [],
-    url: "https://swapi.co/api/people/1/",
-    vehicles: []
-  }
-];
-
+const characters = props => {
+  return (
+    <div className='char-name'>
+    <p>{props.starwarsChars.name}</p>
+    </div>
+  )
+}
 export default characters;
